3882ab2e08ba21e61d8c94c593b5e6c92629d973
Abdiaziz <abdiazizsaman394@gmail.com>
2023-11-27 15:07:23 +0300

    Changes made

--- Challenge_1/time_conversion.py
+++ Challenge_1/time_conversion.py
@@ -1,12 +1,5 @@
 def convert_to_24_hour(hour, minute, period):
-    """
-    Converts 12-hour time format to 24-hour time format.
-
-    :param hour: int, the hour in 12-hour format (1-12)
-    :param minute: int, the minute (0-59)
-    :param period: str, either "am" or "pm"
-    :return: str, time in 24-hour format
-    """
+   
     # Adjust the hour for 12 AM and 12 PM
     if hour == 12:
         hour = 0 if period == 'am' else 12
--- Challenge_2/positive_numbers.py
+++ Challenge_2/positive_numbers.py
@@ -8,7 +8,7 @@ def two_positive_numbers(a, b, c):
     return count == 2
 
 print(two_positive_numbers(8, 4, -3))  
-print(two_positive_numbers(-4, 3, 8))  
+print(two_positive_numbers(-4, 3, 0))  
 
 
 # Path: Challenge_2/positive_numbers.py
